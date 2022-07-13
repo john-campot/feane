c320e81269a06429f87b0a7d3e7496f32d8e6c44
john-campot <johncampo@gmail.com>
2022-07-12 21:33:17 -0300

    desafio

--- js/carteles.js
+++ js/carteles.js
@@ -1,8 +1,27 @@
 alert ("Hola, bienvenido a cafedel");
 console.log ("Hola, bienvenido a cafedel");
 
-let nombre = prompt("Introduzca su nombre:");
-let edad = prompt("Que edad tienes?");
+function obtenerDatos() {
+    let nombre = prompt("introduzca su nombre:");
+    let edad = prompt("introduzca su edad:"); 
+    alert (`tu nombre es ${nombre} y tu edad es ${edad}`);
+}
+
+function mayorDeEdad() {
+    let edad = prompt("introduzca su edad"); 
+    if  (edad >= 18) {
+        alert ("Bienvenido puedes ingresar a nuestro sitio!");
+    } else  {
+      alert ("Disculpa eres menor de edad no puedes ingresar a nuestro sitio!")         
+    };
+};
+
+mayorDeEdad();    
+
+sumar();
+sumarConParametros(4, 6);
+obtenerDatos(); 
+
 
 
 
